5e5e2c4420589583b0223fef9fb4e9540c124a21
Oleg Sannikov <olegsannikov@icerockdev.com>
2017-08-07 14:34:08 +0700

    Add custom blend filters

---
+++ gpuimage-editor/Filters/IRGPUImageBlendFilters.h
@@ -0,0 +1,56 @@
+//
+//  IRGPUImageNormalBlendFilter.h
+//  gpuimage-editor
+//
+//  Created by Oleg Sannikov on 8/5/17.
+//  Copyright © 2017 IceRock Development. All rights reserved.
+//
+
+#import <GPUImage/GPUImage.h>
+#import "IRGPUImageOpacityBlendFilter.h"
+
+@interface IRGPUImageNormalBlendFilter : IRGPUImageOpacityBlendFilter
+@end
+
+@interface IRGPUImageAddBlendFilter : IRGPUImageOpacityBlendFilter
+@end
+
+@interface IRGPUImageAverageBlendFilter : IRGPUImageOpacityBlendFilter
+@end
+
+@interface IRGPUImageColorBurnBlendFilter : IRGPUImageOpacityBlendFilter
+@end
+
+@interface IRGPUImageColorDodgeBlendFilter : IRGPUImageOpacityBlendFilter
+@end
+
+@interface IRGPUImageDarkenBlendFilter : IRGPUImageOpacityBlendFilter
+@end
+
+@interface IRGPUImageDifferenceBlendFilter : IRGPUImageOpacityBlendFilter
+@end
+
+@interface IRGPUImageDivideBlendFilter : IRGPUImageOpacityBlendFilter
+@end
+
+@interface IRGPUImageExclusionBlendFilter : IRGPUImageOpacityBlendFilter
+@end
+
+@interface IRGPUImageHueBlendFilter : IRGPUImageOpacityBlendFilter
+@end
+
+@interface IRGPUImageLightenBlendFilter : IRGPUImageOpacityBlendFilter
+@end
+
+@interface IRGPUImageMultiplyBlendFilter : IRGPUImageOpacityBlendFilter
+@end
+
+@interface IRGPUImageOverlayBlendFilter : IRGPUImageOpacityBlendFilter
+@end
+
+@interface IRGPUImageScreenBlendFilter : IRGPUImageOpacityBlendFilter
+@end
+
+@interface IRGPUImageSubtractBlendFilter : IRGPUImageOpacityBlendFilter
+@end
+
